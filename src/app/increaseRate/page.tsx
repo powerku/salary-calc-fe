aad740884f743662e08842a4b1471273fff75658
griffin <griffin.ku@kakaocorp.com>
2023-10-13 02:37:01 +0900

    fix: 연봉 인상률 계산기 폼 구조로 변경

--- src/app/increaseRate/page.tsx
+++ src/app/increaseRate/page.tsx
@@ -1,8 +1,29 @@
 "use client";
 import {useState} from "react";
+import {useForm} from "react-hook-form";
+
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+import {Button} from "@/components/ui/button";
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import NumericTextBox from "@/components/NumericTextBox";
 
+
+const FormSchema = z.object({
+    before: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+    current: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+})
+
 export default function Home() {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+    })
+
         const [beforeSalary, setBeforeSalary] = useState('0');
         const [afterSalary, setAfterSalary] = useState('0');
         const [rate, setRate] = useState(0);
@@ -23,26 +44,44 @@ export default function Home() {
             setRate(0);
         }
 
-        return (
-        <>
-            <div className="w-full">
+    return (
+        <div className="w-full">
+            <Form {...form}>
                 <form className="my-0 mx-auto" style={{width: "300px"}}>
-                    <label className="mr-10">직전연봉</label>
-                    <NumericTextBox value={beforeSalary} setValue={setBeforeSalary}/>
-                    <span className="ml-1">원</span>
-                    <label className="mr-10">현재연봉</label>
-                    <NumericTextBox value={afterSalary} setValue={setAfterSalary}/>
-                    <span className="ml-1">원</span>
+                    <FormField
+                        control={form.control}
+                        name="before"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>직전연봉</FormLabel>
+                                <FormControl>
+                                    <NumericTextBox value={beforeSalary} setValue={setBeforeSalary}/>
+                                </FormControl>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="current"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>현재연봉</FormLabel>
+                                <FormControl>
+                                    <NumericTextBox value={afterSalary} setValue={setAfterSalary}/>
+                                </FormControl>
+                            </FormItem>
+                        )}
+                    />
+                <FormDescription>
+                    인상률은 {rate}%입니다.
+                </FormDescription>
+                <FormMessage />
+                <div className="my-0 mx-auto text-center" style={{width: "300px"}}>
+                    <Button variant="ghost" onClick={resetBtnClickHandler} className="border mr-1">초기화</Button>
+                    <Button onClick={calcBtnClickHandler} className="text-white">인상률 계산</Button>
+                </div>
                 </form>
-                <div className="my-0 mx-auto text-center" style={{width: "300px"}}>
-                    <button onClick={resetBtnClickHandler} style={{width: "100px"}} className="border rounded-md p-1 text-center mr-1">초기화</button>
-                    <button onClick={calcBtnClickHandler} style={{width: "100px"}} className="border rounded-md p-1 text-center bg-primary text-white">인상률 계산</button>
-                </div>
-                <div className="my-0 mx-auto text-center" style={{width: "300px"}}>
-                    <p className="m-0 text-xl">인상률은 {rate}%입니다.</p>
-                </div>
-            </div>
-
-        </>
-    )
+            </Form>
+        </div>
+    );
 }
