e54e8c32fc14977edb65c8999483106b436e3c22
griffin.ku <griffin.ku@kakaocorp.com>
2024-01-13 21:10:55 +0900

    refactor: 불필요한 코드 삭제

--- src/app/(speed)/speed1/page.tsx
+++ src/app/(speed)/speed1/page.tsx
@@ -5,7 +5,6 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -21,6 +20,12 @@ import NumericTextBox from "@/components/NumericTextBox";
 const FormSchema = z.object({
   speed: z.string().min(2, {}),
 });
+
+type Time = {
+  hour: string;
+  min: string;
+  sec: string;
+};
 
 export default function Home() {
   const form = useForm<z.infer<typeof FormSchema>>({
@@ -38,65 +43,19 @@ export default function Home() {
     });
   }, [form, watchCurrent]);
 
-  const [speed, setSpeed] = useState<number>(10); // form.getValues("current") as number
-  const [time1Km, setTime1km] = useState<{
-    hour: string;
-    min: string;
-    sec: string;
-  }>({
+  const initialTime = {
     hour: "00",
     min: "00",
     sec: "00",
-  });
-  const [time3Km, setTime3km] = useState<{
-    hour: string;
-    min: string;
-    sec: string;
-  }>({
-    hour: "00",
-    min: "00",
-    sec: "00",
-  });
-  const [time5Km, setTime5km] = useState<{
-    hour: string;
-    min: string;
-    sec: string;
-  }>({
-    hour: "00",
-    min: "00",
-    sec: "00",
-  });
-  const [time10Km, setTime10km] = useState<{
-    hour: string;
-    min: string;
-    sec: string;
-  }>({
-    hour: "00",
-    min: "00",
-    sec: "00",
-  });
-  const [timeHalf, setTimeHalf] = useState<{
-    hour: string;
-    min: string;
-    sec: string;
-  }>({
-    hour: "00",
-    min: "00",
-    sec: "00",
-  });
-  const [timeFull, setTimeFull] = useState<{
-    hour: string;
-    min: string;
-    sec: string;
-  }>({
-    hour: "00",
-    min: "00",
-    sec: "00",
-  });
-
-  const onReset = (e: React.MouseEvent<HTMLElement>) => {
-    form.reset();
   };
+
+  const [speed, setSpeed] = useState<number>(10); // form.getValues("current") as number
+  const [time1Km, setTime1km] = useState<Time>(initialTime);
+  const [time3Km, setTime3km] = useState<Time>(initialTime);
+  const [time5Km, setTime5km] = useState<Time>(initialTime);
+  const [time10Km, setTime10km] = useState<Time>(initialTime);
+  const [timeHalf, setTimeHalf] = useState<Time>(initialTime);
+  const [timeFull, setTimeFull] = useState<Time>(initialTime);
 
   const onChange = (value: any) => {
     form.setValue("speed", value[0].toString());
@@ -133,61 +92,24 @@ export default function Home() {
     const timeHalf = time1Km * 21.0975;
     const timeFull = time1Km * 42.195;
 
-    setTime1km({
-      hour: format(Math.floor(time1Km)),
-      min: format(
-        time1Km >= 1
-          ? Math.floor((time1Km - Math.floor(time1Km)) * 60)
-          : Math.floor(time1Km * 60),
-      ),
-      sec: format(Math.floor((time1Km * 60 - Math.floor(time1Km * 60)) * 60)),
-    });
-    setTime3km({
-      hour: format(Math.floor(time3Km)),
-      min: format(
-        time3Km >= 1
-          ? Math.floor((time3Km - Math.floor(time3Km)) * 60)
-          : Math.floor(time3Km * 60),
-      ),
-      sec: format(Math.floor((time3Km * 60 - Math.floor(time3Km * 60)) * 60)),
-    });
-    setTime5km({
-      hour: format(Math.floor(time5Km)),
-      min: format(
-        time5Km >= 1
-          ? Math.floor((time5Km - Math.floor(time5Km)) * 60)
-          : Math.floor(time5Km * 60),
-      ),
-      sec: format(Math.floor((time5Km * 60 - Math.floor(time5Km * 60)) * 60)),
-    });
-    setTime10km({
-      hour: format(Math.floor(time10Km)),
-      min: format(
-        time10Km >= 1
-          ? Math.floor((time10Km - Math.floor(time10Km)) * 60)
-          : Math.floor(time10Km * 60),
-      ),
-      sec: format(Math.floor((time10Km * 60 - Math.floor(time10Km * 60)) * 60)),
-    });
-    setTimeHalf({
-      hour: format(Math.floor(timeHalf)),
-      min: format(
-        timeHalf >= 1
-          ? Math.floor((timeHalf - Math.floor(timeHalf)) * 60)
-          : Math.floor(timeHalf * 60),
-      ),
-      sec: format(Math.floor((timeHalf * 60 - Math.floor(timeHalf * 60)) * 60)),
-    });
-    console.log(timeFull);
-    setTimeFull({
-      hour: format(Math.floor(timeFull)),
-      min: format(
-        timeFull >= 1
-          ? Math.floor((timeFull - Math.floor(timeFull)) * 60)
-          : Math.floor(timeFull * 60),
-      ),
-      sec: format(Math.floor((timeFull * 60 - Math.floor(timeFull * 60)) * 60)),
-    });
+    const getTime = (value: number) => {
+      return {
+        hour: format(Math.floor(value)),
+        min: format(
+          value >= 1
+            ? Math.floor((value - Math.floor(value)) * 60)
+            : Math.floor(value * 60),
+        ),
+        sec: format(Math.floor((value * 60 - Math.floor(value * 60)) * 60)),
+      };
+    };
+
+    setTime1km(getTime(time1Km));
+    setTime3km(getTime(time3Km));
+    setTime5km(getTime(time5Km));
+    setTime10km(getTime(time10Km));
+    setTimeHalf(getTime(timeHalf));
+    setTimeFull(getTime(timeFull));
   };
 
   useEffect(() => {
@@ -238,10 +160,12 @@ export default function Home() {
             10km 기록: {time10Km.hour}시간 {time10Km.min}분 {time10Km.sec}초
           </FormDescription>
           <FormDescription className="text-base">
-            Half 기록: {timeHalf.hour}시간 {timeHalf.min}분 {timeHalf.sec}초
+            Half(21.0975.km) 기록: {timeHalf.hour}시간 {timeHalf.min}분{" "}
+            {timeHalf.sec}초
           </FormDescription>
           <FormDescription className="text-base">
-            Full 기록: {timeFull.hour}시간 {timeFull.min}분 {timeFull.sec}초
+            Full(42.105km) 기록: {timeFull.hour}시간 {timeFull.min}분{" "}
+            {timeFull.sec}초
           </FormDescription>
         </div>
       </form>
