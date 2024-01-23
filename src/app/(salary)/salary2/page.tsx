61f78ef154b599f3a5e534fb9d43723e863de540
griffin.ku <griffin.ku@kakaocorp.com>
2024-01-24 08:58:11 +0900

    feat: 속도별 페이스 계산기 메뉴 추가

--- src/app/(salary)/salary2/page.tsx
+++ src/app/(salary)/salary2/page.tsx
@@ -105,8 +105,11 @@ export default function Home() {
           browsingtopics
         ></iframe>
         <span className="text-center text-sm">
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
           "이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의
-          수수료를 제공받습니다."
+          수수료를
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          제공받습니다."
         </span>
       </form>
     </Form>
